320a954e1783a8a6ed6d62ba9aac291ea711ddac
querubcastillo <cardstillo@gmail.com>
2023-02-24 20:34:13 -0600

    Update navbar

--- src/components/NavbarDisplay/NavbarDisplay.tsx
+++ src/components/NavbarDisplay/NavbarDisplay.tsx
@@ -13,13 +13,13 @@ const NavbarDisplay: React.FC = () => {
   };
 
   return (
-    <nav className={`navbar navbar-expand-md navbar-${darkMode ? 'dark' : 'light'} bg-${darkMode ? 'dark' : 'light'}`}>
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+    <nav className={`navbar navbar-expand-md navbar-${darkMode ? 'dark' : 'light'} bg-${darkMode ? 'dark' : 'light'}`} style={{padding: '2vh 2vw'}}>
+      <div className="container-fluid" style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <a className="navbar-brand me-auto" href="#">
           { darkMode ? <img src="src\logo.svg" alt="Logo" width="80" height="80" className="d-inline-block align-text-top me-2" /> : <img src="src\logo-black.svg" alt="Logo" width="80" height="80" className="d-inline-block align-text-top me-2" /> }
         </a>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul className="navbar-nav d-flex" style={{justifyContent: 'space-between', alignItems: 'center', width: '30vw'}}>
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
@@ -31,14 +31,8 @@ const NavbarDisplay: React.FC = () => {
             </li>
           </ul>
         </div>
-        <div className="d-flex">
-          <a className="nav-link" href="#">
-            <i className="bi bi-linkedin me-3"></i>
-          </a>
-          <a className="nav-link" href="#">
-            <i className="bi bi-github"></i>
-          </a>
-          <Switch onChange={handleToggleDarkMode} checked={darkMode} handleDiameter={15} height={20} onColor="#387373" offColor="#97A6A0"  />
+        <div className="d-flex align-items-center">
+          <Switch onChange={handleToggleDarkMode} checked={darkMode} handleDiameter={15} height={20} onColor="#387373" offColor="#97A6A0" />
         </div>
       </div>
     </nav>
