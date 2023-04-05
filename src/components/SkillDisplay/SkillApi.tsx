d28e028b7ba2c4344cc5b4021cd475d04bfe41cf
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-04-04 18:13:34 -0600

    Update SkillApi.tsx

--- src/components/SkillDisplay/SkillApi.tsx
+++ src/components/SkillDisplay/SkillApi.tsx
@@ -11,7 +11,7 @@ interface Skill {
 // Obtiene las habilidades con Axios
 export const getSkills = async (): Promise<Skill[]> => {
   try {
-    const response = await axios.get<Skill[]>('http://20.77.66.228:8088/api/skill/?format=json');
+    const response = await axios.get<Skill[]>('https://queb.online:8088/api/skill/?format=json');
     return response.data;
   } catch (error) {
     console.error(error);
