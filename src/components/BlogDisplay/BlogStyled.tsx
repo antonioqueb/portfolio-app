bc04575235d805cac78948f1051b150cb0a55c27
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-04-06 17:15:29 +0000

    up

--- src/components/BlogDisplay/BlogStyled.tsx
+++ src/components/BlogDisplay/BlogStyled.tsx
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
+interface BlogContainerProps {
+  isDarkMode?: boolean;
+}
+
 export const Styled = {
-  BlogContainer: styled.div`
+  BlogContainer: styled.div<BlogContainerProps>`
     padding: 2rem;
+    background-color: ${({ isDarkMode }) => (isDarkMode ? '#333' : 'white')};
+    transition: background-color 0.5s ease;
   `,
   CategorySelect: styled.select`
     margin: 1rem 0;
@@ -12,9 +18,12 @@ export const Styled = {
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
     grid-gap: 2rem;
   `,
-  Post: styled.div`
-    border: 1px solid #ccc;
+  Post: styled.div<BlogContainerProps>`
+    border: 1px solid ${({ isDarkMode }) => (isDarkMode ? '#777' : '#ccc')};
     border-radius: 4px;
     padding: 1rem;
+    background-color: ${({ isDarkMode }) => (isDarkMode ? '#444' : 'white')};
+    color: ${({ isDarkMode }) => (isDarkMode ? 'white' : 'black')};
+    transition: background-color 0.5s ease, color 0.5s ease, border 0.5s ease;
   `,
 };
