a4c7b8e2ea94ef3730530518b2886293b0667726
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-04-06 18:32:39 +0000

    Up

--- src/components/BlogDisplay/BlogPost.tsx
+++ src/components/BlogDisplay/BlogPost.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { BlogPostStyled } from './BlogPostStyled';
+import BlogPostStyled from './BlogPostStyled';
 
 interface Post {
   id: number;
