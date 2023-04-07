7eeb9c66da4dfcd941e986a933c3888ed8fa5a09
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-04-07 20:38:15 +0000

    Up

--- src/pages/Blog/blogdetail.tsx
+++ src/pages/Blog/blogdetail.tsx
@@ -9,7 +9,7 @@ const BlogDetail: React.FC = () => {
     return (
       <>
       <NavbarDisplay/>
-      <BlogPost/>
+      <BlogPost postId={1} setPostId={setId} />
       <FooterDisplay/>
       </>
     );
