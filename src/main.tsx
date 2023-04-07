09ad91de0e690e43c64dd6daee15544fce4a6877
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-04-07 16:06:41 +0000

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -3,38 +3,31 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import { selectIsDarkMode } from './reducers/darkmode/darkmodeSlices';
 import { useSelector } from 'react-redux';
-import './index.css'; // Archivo CSS que contiene las definiciones de las clases bg-dark y light
-
+import './index.css';
 
 //Pages
-import Home from '../src/pages/Home/Home'
-import Blog from '../src/pages/Blog/blog'
-
+import Home from '../src/pages/Home/Home';
+import Blog from '../src/pages/Blog/blog';
+import PostDetail from '../src/pages/PostDetail/PostDetail';
 
 //Redux
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 
 //Router
-import {createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home/>,
-    errorElement: <h1>UPS... EROR 404</h1>
-  },
-  {
-    path: '/blog',
-    element: <Blog/>,
-  },
-]);
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   const isDarkMode = useSelector(selectIsDarkMode);
   return (
-    <div className={isDarkMode ? 'bg-dark' : 'light'}> {/* Agrega la clase correspondiente */}
-      <RouterProvider router={router} />
+    <div className={isDarkMode ? 'bg-dark' : 'light'}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:postId" element={<PostDetail />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
