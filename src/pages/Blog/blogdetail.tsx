a63fdeaa3c1ea30e8da9e6bf293b749a8e6a8ffc
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-04-07 16:35:19 +0000

    Up

--- src/pages/Blog/blogdetail.tsx
+++ src/pages/Blog/blogdetail.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavbarDisplay from '../../components/NavbarDisplay/NavbarDisplay';
 import FooterDisplay from '../../components/FooterDisplay/FooterDisplay';
-import BlogDisplay from '../../components/BlogDisplay/BlogPost';
+import BlogPost from '../../components/BlogDisplay/BlogPost';
 
 const BlogDetail: React.FC = () => {
   
