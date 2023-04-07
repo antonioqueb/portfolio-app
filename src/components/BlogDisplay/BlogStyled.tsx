73ba49858d3d9ca4950e9772230fe339b3c03fe5
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-04-07 20:20:53 +0000

    Upgrade

--- src/components/BlogDisplay/BlogStyled.tsx
+++ src/components/BlogDisplay/BlogStyled.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 export const Styled = {
   BlogContainer: styled.div<{ isDarkMode?: boolean }>`
     max-width: 1200px;
@@ -72,84 +74,60 @@ export const Styled = {
     }
   `,
   BlogPostContainer: styled.div<{ isDarkMode?: boolean; loaded: boolean }>`
-  max-width: 1200px;
-  margin: 0 auto;
-  font-family: 'Roboto', sans-serif;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 2rem;
-  opacity: ${({ loaded }) => (loaded ? 1 : 0)};
-  transform: ${({ loaded }) => (loaded ? 'translateY(0)' : 'translateY(-20px)')};
-  transition: opacity 500ms, transform 500ms;
-
-  h1 {
-    font-size: 3rem;
-    margin-bottom: 2rem;
-    color: #ff8c00;
-    text-transform: uppercase;
-    letter-spacing: 3px;
-    text-align: center;
-  }
-
-  .post-content {
-    max-width: 800px;
-    width: 100%;
-    font-size: 1.2rem;
-    color: ${({ isDarkMode }) => (isDarkMode ? '#0A0A0A' : '#0a0a0')};
-    line-height: 1.5;
-    text-align: justify;
-
-    h2 {
-      font-size: 2rem;
-      margin: 2rem 0 1rem 0;
-      color: #333;
-      text-align: center;
-    }
-
-    p {
-      margin-bottom: 1rem;
-    }
-
-    ul,
-    ol {
-      margin-left: 2rem;
-      margin-bottom: 1rem;
-
-      li {
-        margin-bottom: 0.5rem;
-      }
-    }
-
-    img {
-      max-width: 100%;
-      border-radius: 15px;
-      margin: 2rem 0;
-      box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.1);
-    }
-  }
-
-  .post-meta {
-    margin-top: 2rem;
+    max-width: 1200px;
+    margin: 0 auto;
+    font-family: 'Roboto', sans-serif;
     display: flex;
     flex-direction: column;
     align-items: center;
-    font-size: 1.2rem;
-    color: ${({ isDarkMode }) => (isDarkMode ? '#0A0A0A' : '#0a0a0')};
+    padding: 2rem;
+    opacity: ${({ loaded }) => (loaded ? 1 : 0)};
+    transform: ${({ loaded }) => (loaded ? 'translateY(0)' : 'translateY(-20px)')};
+    transition: opacity 500ms, transform 500ms;
 
-    p {
-      margin-bottom: 0.5rem;
-    }
-
-    .category {
-      font-weight: bold;
-      margin-bottom: 1rem;
+    h1 {
+      font-size: 3rem;
+      margin-bottom: 2rem;
+      color: #ff8c00;
       text-transform: uppercase;
+      letter-spacing: 3px;
+      text-align: center;
     }
 
-    .date {
-      font-style: italic;
-    }
-  }
-`
-}
+    .post-content {
+      max-width: 800px;
+      width: 100%;
+      font-size: 1.2rem;
+      color: ${({ isDarkMode }) => (isDarkMode ? '#0A0A0A' : '#0a0a0')};
+      line-height: 1.5;
+      text-align: justify;
+  
+      h2 {
+        font-size: 2rem;
+        margin: 2rem 0 1rem 0;
+        color: #333;
+        text-align: center;
+      }
+  
+      p {
+        margin-bottom: 1rem;
+      }
+  
+      ul,
+      ol {
+        margin-left: 2rem;
+        margin-bottom: 1rem;
+  
+        li {
+          margin-bottom: 0.5rem;
+        }
+      }
+  
+      img {
+        max-width: 100%;
+        border-radius: 15px;
+        margin: 2rem 0;
+        box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.1);
+      }
+    `
+}  
