ddd5628174518e35a9a278c9a7768c15dd8b03d1
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-04-09 16:39:32 +0000

    Up

--- src/pages/Blog/blogdetail.tsx
+++ src/pages/Blog/blogdetail.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import NavbarDisplay from '../../components/NavbarDisplay/NavbarDisplay';
 import FooterDisplay from '../../components/FooterDisplay/FooterDisplay';
 import BlogPost from '../../components/BlogDisplay/BlogPost';
-import { useParams } from 'react-router-dom';
 
-
+interface Params {
+  postId: string;
+}
 
 const BlogDetail: React.FC = () => {
-  const { postId } = useParams<{ postId: string }>();
+  const { postId } = useParams<Params>();
+  const postIdAsNumber = parseInt(postId, 10);
 
-  
-    return (
-      <>
-      <NavbarDisplay/>
-      <BlogPost postId={postId} />
-      <FooterDisplay/>
-      </>
-    );
+  return (
+    <>
+      <NavbarDisplay />
+      <BlogPost postId={postIdAsNumber} />
+      <FooterDisplay />
+    </>
+  );
 };
 
 export default BlogDetail;
