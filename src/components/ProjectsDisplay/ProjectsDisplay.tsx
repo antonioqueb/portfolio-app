3bb247386ee00fd6333052f3c32247b6774097a7
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-04-13 03:43:57 +0000

    Update

--- src/components/ProjectsDisplay/ProjectsDisplay.tsx
+++ src/components/ProjectsDisplay/ProjectsDisplay.tsx
@@ -32,26 +32,26 @@ const Projects = () => {
   return (
     <Styled.ProjectsContainer  id="projects" isDarkMode={isDarkMode}>
       <br/>
-      <Styled.TitleP style={{ fontSize: '2.5rem', fontWeight: 700 }} isDarkMode={isDarkMode}>Projects</Styled.TitleP>
-      <Styled.TitleP style={{ fontSize: '1.5rem', fontWeight: 400 }} isDarkMode={isDarkMode}>Here you will find my latest projects.</Styled.TitleP>
+      <Styled.TitleP style={{  fontWeight: 700 }} isDarkMode={isDarkMode}>Projects</Styled.TitleP>
+      <Styled.TitleP style={{  fontWeight: 400 }} isDarkMode={isDarkMode}>Here you will find my latest projects.</Styled.TitleP>
       {projects.map((project) => (
         <Styled.ProjectContainer key={project.id} isDarkMode={isDarkMode}>
           <Styled.Image src={project.image_url} alt={project.title} />
           <Styled.Description>
             <Styled.Title
-            style={{ fontSize: '2.5rem', fontWeight: 700 }}
+            style={{ fontWeight: 700 }}
             
             >{project.title}</Styled.Title>
             <div
-            style={{ fontSize: '1.5rem', fontWeight: 400 }}
+            style={{  fontWeight: 400 }}
             >{project.details}</div>
             <Styled.ButtonRow>
-              <Styled.DemoButton style={{ fontSize: '1.5rem', fontWeight: 600 }} isDarkMode={isDarkMode}>
+              <Styled.DemoButton style={{ fontWeight: 600 }} isDarkMode={isDarkMode}>
                 <a href={project.demo_url} target="_blank" rel="noopener noreferrer">
                   Demo
                 </a>
               </Styled.DemoButton>
-              <Styled.GitHubButton style={{ fontSize: '1.5rem', fontWeight: 600 }} isDarkMode={isDarkMode}>
+              <Styled.GitHubButton style={{  fontWeight: 600 }} isDarkMode={isDarkMode}>
                 <a href={project.github_url} target="_blank" rel="noopener noreferrer">
                   GitHub
                 </a>
