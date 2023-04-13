22b9ef14cd0eaa5d0362f588099dc2920b692803
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-04-13 04:01:30 +0000

    Up

--- src/components/ProjectsDisplay/ProjectsStyled.tsx
+++ src/components/ProjectsDisplay/ProjectsStyled.tsx
@@ -84,11 +84,12 @@ ProjectContainer : styled.div<{ isDarkMode: boolean }>`
   background-color: ${({ isDarkMode }) => (isDarkMode ? "#dark" : "#fffff")};
 
   @media (max-width: 1024px) {
-    width: 90%;
+    width: 80%;
   }
 
   @media (max-width: 768px) {
-    width: 100%;
+    flex-direction: column;
+    width: 95%;
   }
 
   @media (max-width: 480px) {
